34a607dde267499bbd29e0f17fbbbac733ced905
John Cooper <31467519+robustNan@users.noreply.github.com>
2019-03-05 20:52:33 -0600

    Update index.js

--- src/script/index.js
+++ src/script/index.js
@@ -3,8 +3,8 @@ import Arithmetic from './arithmetic';
 const btn = document.querySelector('#btn');
 const input = document.querySelector('#input');
 const output = document.querySelector('#output');
-const arithmetic = new Arithmetic();
+const arith = new arithmetic.Main();
 
 btn.addEventListener('click', () => {
-    output.value = arithmetic.calculate(input.value);
+    output.value = arith.calculate(input.value);
 }, false);
